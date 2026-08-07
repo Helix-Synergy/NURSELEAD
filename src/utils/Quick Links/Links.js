8d39f979a2d754ffb7ed20fa7ca84908109f73fb
Shashiyadav8 <vallalashashi9848@gmail.com>
2026-08-07 16:12:15 +0530

    Fix brochure downloads, update banners, and correct Quick Links

--- src/utils/Quick Links/Links.js
+++ src/utils/Quick Links/Links.js
@@ -1,5 +1,4 @@
 import { Facebook, Twitter, Instagram, Youtube, Linkedin } from "lucide-react";
-import { FaReddit } from "react-icons/fa"
 
 const XIcon = ({ size = 18 }) => (
   <svg
@@ -44,14 +43,8 @@ export const socialLinks = [
   {
     id: "linkedin",
     name: "LinkedIn",
-    url: "https://www.linkedin.com/company/helixconferences",
+    url: "https://www.linkedin.com/in/sri-r-b7791a224/",
     icon: <Linkedin size={18} />,
-  },
-  {
-    id: "reddit",
-    name: "Reddit",
-    url: "https://www.reddit.com/user/Salty-Ad6381/",
-    icon: <FaReddit size={18} />,
   },
 ];
 
